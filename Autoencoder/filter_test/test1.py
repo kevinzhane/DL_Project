df05a1f42fda50c8b244a33a31ce6038e5b1f94c
kevin-home <a3036a3036@gmail.com>
2020-04-24 00:37:38 +0800

    Upload loop callback function

--- Autoencoder/filter_test/test1.py
+++ Autoencoder/filter_test/test1.py
@@ -28,21 +28,20 @@ for i in range(1,2):
     filter_num = i*4
     ## Build the Convolution autoencoders model
 
-    x = Conv2D(filter_num, (3, 3), activation='relu', padding='same')(input_img)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
     x = MaxPooling2D((2, 2), padding='same')(x)
-
-    x = Conv2D(filter_num, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
     encoded = MaxPooling2D((2, 2), padding='same')(x)
-    #x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    #encoded = MaxPooling2D((2, 2), padding='same')(x)
 
 
-    x = Conv2D(filter_num, (3, 3), activation='relu', padding='same')(encoded)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(filter_num, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
-    #x = Conv2D(16, (3, 3), activation='relu')(x)
-    #x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu')(x)
+    x = UpSampling2D((2, 2))(x)
     decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
 
